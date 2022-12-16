3bd189bc08f6ef53c1c57374fa7393fba15706d8
Saurabh Anand <anandsaurabh17@gmail.com>
2022-12-15 20:56:43 -0700

    created custom library housing, added setup.py file, updated requirements file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app= Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return "Starting Machine Learning Project"
+    return "Starting CI/CD Pipeline"
 
 
 if __name__=="__main__":
